08a3ec18106c9872a05a16ef614c7d6160770230
elomarii <i.ibrahim.elomari@gmail.com>
2024-11-27 00:55:10 +0100

    sanitize requested path

--- parser.go
+++ parser.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"strings"
+	"path"
 )
-
-// TODO parse HTTP request and
-// -> identify HTTP version
-// -> requested resource and method
-// -> parse headers into a custom struct
 
 type HTTPReq struct {
 	method, resource, version string
@@ -23,11 +19,13 @@ func ParseHTTPRequest(request string) HTTPReq {
 	lines := strings.Split(request, "\n")
 	var req HTTPReq
 
+	// method, resource, version
 	head := strings.Split(lines[0], " ")
 	req.method = head[0]
-	req.resource = head[1]
+	req.resource = path.Clean(head[1])
 	req.version = head[2]
 
+	// http headers
 	headers := lines[1:len(lines)-1] // last element is blank
 	for _, line := range headers {
 		l := strings.SplitN(line, ": ", 2)
@@ -42,3 +40,4 @@ func ParseHTTPRequest(request string) HTTPReq {
 	}
 	return req
 }
+
